41a45884fedf38227f41c83f802254e8c9e5c0f2
JuniorPeixoto <juniorpeixotofraga@gmail.com>
2025-12-04 13:11:48 -0300

    fix: fix kernel_version variable

--- kernel/config-build-env.sh
+++ kernel/config-build-env.sh
@@ -11,11 +11,12 @@ sudo ln -sf /usr/bin/ccache /usr/local/bin/gcc
 sudo ln -sf /usr/bin/ccache /usr/local/bin/g++
 
 # download and extract the kernel source code
-wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${kernel-version}.tar.xz" \
-  && tar -xf "linux-${kernel-version}.tar.xz"
+cd "$HOME" || exit
+wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${kernel_version}.tar.xz" \
+  && tar -xf "linux-${kernel_version}.tar.xz"
 
 # enter in the source kernel directory
-cd "linux-${kernel-version}" || exit
+cd "$HOME/linux-${kernel_version}" || exit
 
 [ ! -e "$HOME/git/oracle-vm-scripts/kernel/config" ] \
   && cp "$HOME/git/oracle-vm-scripts/kernel/config" "$HOME/linux-*/.config"
