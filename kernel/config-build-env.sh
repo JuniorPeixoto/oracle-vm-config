1d131f8a1f9a4e988d56d76f5f8c473b221340cf
JuniorPeixoto <juniorpeixotofraga@gmail.com>
2025-12-04 13:09:05 -0300

    fix: fix missing packages(bc,glew)

--- kernel/config-build-env.sh
+++ kernel/config-build-env.sh
@@ -1,21 +1,21 @@
 #!/bin/env sh
 
 # define the kernel version
-set kernel-version 6.17.10
+kernel_version=6.17.10
 
 # install necessary packages
-sudo apt install make gcc flex bison htop nvim ccache
+sudo apt install -y make git gcc flex bison bc libglew-dev htop neovim ccache
 
 # config ccache
-sudo ln -s /usr/bin/ccache /usr/local/bin/gcc
-sudo ln -s /usr/bin/ccache /usr/local/bin/g++
+sudo ln -sf /usr/bin/ccache /usr/local/bin/gcc
+sudo ln -sf /usr/bin/ccache /usr/local/bin/g++
 
 # download and extract the kernel source code
-wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$(kernel-version).tar.xz" \
-  && tar -xf "linux-$(kernel-version).tar.xz"
+wget "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${kernel-version}.tar.xz" \
+  && tar -xf "linux-${kernel-version}.tar.xz"
 
 # enter in the source kernel directory
-cd "linux-$(kernel-version)" || exit
+cd "linux-${kernel-version}" || exit
 
 [ ! -e "$HOME/git/oracle-vm-scripts/kernel/config" ] \
   && cp "$HOME/git/oracle-vm-scripts/kernel/config" "$HOME/linux-*/.config"
